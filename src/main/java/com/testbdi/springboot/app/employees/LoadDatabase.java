aebdf86bebdcbfbf76e584fc6e323557c9f54879
chavezibarria <chavezibarria@hotmail.com>
2021-06-29 14:25:21 -0500

    adding import data and script

--- src/main/java/com/testbdi/springboot/app/employees/LoadDatabase.java
+++ src/main/java/com/testbdi/springboot/app/employees/LoadDatabase.java
@@ -1,13 +1,22 @@
 package com.testbdi.springboot.app.employees;
 
-import java.util.Date;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.apache.catalina.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.testbdi.springboot.app.employees.models.dao.EmployeeDao;
 import com.testbdi.springboot.app.employees.models.entity.Employee;
 
@@ -20,14 +29,27 @@ public class LoadDatabase {
 	@Bean
 	CommandLineRunner initDatabase(EmployeeDao employeeDao) {
 		
-		Employee employee1 = new Employee("Pepe", "P", "Pecas", new Date("01/01/2021"), new Date("01/01/2021"));
-		Employee employee2 = new Employee("Calebsio", "D", "Calabacio", new Date("01/01/2021"), new Date("01/01/2021"));
+		try {
+		    
+		    Reader reader = new InputStreamReader(this.getClass()
+		            .getResourceAsStream("/data.json"));
+
+		    List<Employee> employees = new Gson().fromJson(reader, new TypeToken<List<Employee>>() {}.getType());
+
+		    employees.forEach(e -> employeeDao.save(e));
+		    
+		    
+		    reader.close();
+
+		} catch (Exception ex) {
+		    ex.printStackTrace();
+		}
 		
 		return args -> {
 			
-			log.info("Loading " +  employeeDao.save(employee1));
+			log.info("data import succesfull");
 			
-			log.info("Loading " +  employeeDao.save(employee2));
 		};
 	}
+	
 }
